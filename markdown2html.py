007efe687e33c7dd92c73953bfc4d57a5ec44d60
Sofia Cheung <sofiavictoria17@gmail.com>
2020-02-13 13:31:32 -0800

    Code now parses trough the headings markdown syntax

--- markdown2html.py
+++ markdown2html.py
@@ -8,6 +8,8 @@ if __name__ == "__main__":
     import sys
     from os import path
 
+    markD = {"#": "h1", "##": "h2", "###": "h3", "####": "h4", "#####": "h5", "######": "h6"}
+
     if len(sys.argv) < 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         exit(1)
@@ -16,5 +18,12 @@ if __name__ == "__main__":
         sys.stderr.write("Missing " + sys.argv[1] + '\n')
         exit(1)
 
-    with open(sys.argv[1], mode='r') as f:
+    with open(sys.argv[1], mode='r') as fr, open(sys.argv[2], mode='w') as fw:
+        for line in fr:
+            lineSplit = line.split(' ')
+            if lineSplit[0] in markD:
+                tag = markD[lineSplit[0]]
+            toWrite = line.replace(lineSplit[0],"<{}>".format(tag))
+            toWrite = toWrite[:-1] + (" </{}>\n".format(tag))
+            fw.write(toWrite)
         exit(0)
