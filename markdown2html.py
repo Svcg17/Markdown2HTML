70e05eed38c63b012f3df5224b6a1669c78a8f45
Sofia Cheung <sofiavictoria17@gmail.com>
2020-02-14 13:28:14 -0800

    Code now parses paragraph syntax

--- markdown2html.py
+++ markdown2html.py
@@ -20,6 +20,7 @@ if __name__ == "__main__":
 
     with open(sys.argv[1], mode='r') as fr, open(sys.argv[2], mode='w+') as fw:
         first = 0
+        f = 0
         read = fr.readlines()
         for i, line in enumerate(read):
             lineSplit = line.split(' ')
@@ -47,6 +48,19 @@ if __name__ == "__main__":
                         toWrite = "</{}>\n".format(tag)
                         fw.write(toWrite)
                         first = 0
+            else:
+                # paragraphs 
+                if line[0] != "\n":
+                    if not f:
+                        fw.write("<p>\n")
+                        f = 1
+                    fw.write(line)
+                    if i != len(read) - 1 and read[i + 1][0] != "\n" and read[i + 1][0] not in markD:
+                        fw.write("</br>\n")
+                        continue
+                    else: 
+                        fw.write("</p>\n")
+                        f = 0
                 else:
                     fw.write(line)
         exit(0)
